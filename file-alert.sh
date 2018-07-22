6ad1be5d3856d303972492d9e9b0068ebf780fab
rzr1991 <gheorghexvlad@gmail.com>
2018-07-22 17:07:21 +0300

    Update file-alert.sh

--- file-alert.sh
+++ file-alert.sh
@@ -11,7 +11,7 @@ fi
 function filein() {
   filein=($(find $ftpsap/Vendor/Inbound/examplefile.fil -type f -daystart -mtime +2))
   if [ ${#filein[@]} -ne 0 ]; then
-    printf '%s\n' "Check the File Inbound scenario and trigger the transmission to pull the current file if current one is dated last week." "${empwerin[@]}" | mail -s "ALERT: Todays EmpowerInbound file jpmorganin.fil is not on $ftpsaprhi/EMPOWER/Inbound" $inbox
+    printf '%s\n' "Check the File Inbound scenario and trigger the transmission to pull the current file if current one is dated last week." "${filein[@]}" | mail -s "ALERT: Todays Vendor Inbound file examplefile.fil is not on $ftpsap/Vendor/Inbound" $inbox
 #  else
 #    echo -e "The File Inbound file /usr/users/ftpsap/Vendor/Inbound/examplefile.fil exists. You can ignore this message." | mail -s "IGNORE: The Empower Inbound file exists." $inbox
   fi
